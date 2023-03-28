c74ec79a245057712c907cc452fc0135d96a5d55
wesleyfrench <84875225+wesleyfrench@users.noreply.github.com>
2023-03-28 13:42:45 -0500

    Changed Names
    Changed names of files to have a better understanding of the files

--- Collision/Collision.js
+++ Collision/Collision.js
@@ -1,14 +1,7 @@
 window.onload=()=>{
-    console.log("Page loaded - loader load");
     let canvas = document.getElementById("my-canvas")
     var ctx = canvas.getContext("2d");
     ctx.beginPath();
     ctx.arc(100, 75, 50, 0, 2 * Math.PI);
     ctx.stroke();
-
-    var img = new Image();
-    img.onload = function() {
-        ctx.drawImage(img, 0, 0, 600, 400);
-    };
-    img.src = 'Background.png';
 }
